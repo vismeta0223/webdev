bf83c45ea8db65f519e5c7129dd7c0b2dfedeeaf
Vismeta0223 <gamingbuddy123pass@gmail.com>
2024-05-14 11:09:04 +0800

    added channel

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,60 +8,67 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="{{ asset('bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="design/style.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>Home Page</title>
 </head>
 <body>
+  
     
-    <header>
-        <div class="signup">
-            <nav class="navbar navbar-expand-lg bg-body-tertiary" style="background-color: transparent;">
-                <div class="container-fluid">
-                  <a class="navbar-brand" href="#">
-                    <img src="images/logo.png" alt="logo" width="200px">
-                  </a>
-                  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                  </button>
-                  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                      <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
-                      </li>
-                      <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
-                      </li>
-                      <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                          Dropdown
-                        </a>
-                        <ul class="dropdown-menu">
-                          <li><a class="dropdown-item" href="#">Action</a></li>
-                          <li><a class="dropdown-item" href="#">Another action</a></li>
-                          <li><hr class="dropdown-divider"></li>
-                          <li><a class="dropdown-item" href="#">Something else here</a></li>
-                        </ul>
-                      </li>
-                      <li class="nav-item">
-                        <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-                      </li>
-                    </ul>
-                    <form class="d-flex" role="search">
-                      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                      <button class="btn btn-outline-success" type="submit">Search</button>
-                    </form>
-                    <div class="login-register" style="margin-left: 20px; margin-bottom: 17px;">
-                        <button class="btn btn-outline-danger" type="submit">Sign In</button>
-                        <button class="btn btn-outline-success" type="submit">Sign Up</button>
-                    </div>
-                    
-                  </div>
-                </div>
-            </nav>
-        </div>
+    <header class="own-header">
+      <div class="col">
+      </div>
+      <div class=" col center">
+        <a href="#" ><img src="images/logo.png" alt="logo" class="imglogo"></a>
+      </div>
+      <div class="col side-nav">
+      <nav class="own-navbar">
+        <button href="#" class="signin">Sign In</button>
+        <button href="#" class="signup">Sign Up</button>
+      </nav>
+      </div>
+      
     </header>
 
-    <div class="col" style="width: 100%; height: 450px; background-color:brown;">
-        .
+<section class="orange-box"></section>
+    <div class="col "> 
+      <img src="images/back-img.png" alt="backImg" style="width: 100%; height: 750px; position:absolute; z-index: -10; mix-blend-mode: darken; opacity: 0.1;">
+      <div class="container text-center">
+        <div class="row" >
+          <div class="col">
+            
+          </div>
+          <div class="col-6 " style="margin-top: 200px;">
+            <h1 style="font-family: 'Deli', sans-serif; color:white; margin-top: 30px;">Welcome to <span style="color: rgb(100, 35, 4);">Door</span> 2 <span style="color: rgb(100, 35, 4)">Door</span> Delivery Service</h1>
+            <br>
+            <form class="d-flex" role="search">
+              <input class="form-control me-2" type="search" placeholder="Select your delivery location" aria-label="Search" >
+              <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+          </div>
+          <div class="col">
+            
+          </div>
+        </div>
+        <img src="images/rider-img.png" alt="rider"  class="floating-image rider-img" >
+        <img src="images/carry-rider.png" alt="carry"  class="floating-image-2 carry-rider" >
+        <img src="images/pasta.png" alt="pasta"  class="pasta" style=" " >
+        <img src="images/fastfood.png" alt="fastfood"  class="fastfood" style="" >
+        <div class="icon-pop">
+          <img src="images/truck-fast-solid.png" alt="truck"  class=" floating-icon" style="">
+        </div>
+        <div class="icon-pop2">
+          <img src="images/truck-fast-solid.png" alt="truck"  class=" floating-icon2" style="">
+        </div>
+        <div class="icon-pop3">
+          <img src="images/truck-fast-solid.png" alt="truck"  class=" floating-icon3" style="">
+        </div>
+      </div>
     </div>
+</section>
+  <section class="content" style="height: 500px; margin-top:338px">
+    hi
+  </section>
 </body>
 </html>
